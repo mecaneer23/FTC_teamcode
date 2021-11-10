d09d59751f91c6249f7db6ef88458e976e332eb2
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-11-09 19:12:46 -0600

    Update Simple_Mecanum.java

--- Simple_Mecanum.java
+++ Simple_Mecanum.java
@@ -14,7 +14,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class Simple_Mecanum extends LinearOpMode {
+public class GabiTest extends LinearOpMode {
 
     private DcMotor frontLeft;
     private DcMotor backLeft;
@@ -31,14 +31,20 @@ public class Simple_Mecanum extends LinearOpMode {
         double fr;
         double bl;
         double br;
-
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-	CarouselSpinner = hardwareMap.get(DcMotor.class, "CarouselSpinner");
+	    
+        frontLeft = hardwareMap.get(DcMotor.class,
+            "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class,
+            "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class,
+            "frontRight");
+        backRight = hardwareMap.get(DcMotor.class,
+            "backRight");
+        CarouselSpinner = hardwareMap.get(DcMotor.class,
+            "CarouselSpinner");
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+        CarouselSpinner.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
         if (opModeIsActive()) {
@@ -46,45 +52,50 @@ public class Simple_Mecanum extends LinearOpMode {
             backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-	    CarouselSpinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            CarouselSpinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             while (opModeIsActive()) {
                 x = gamepad1.left_stick_x;
                 y = gamepad1.left_stick_y;
                 clockwise = gamepad1.right_stick_x;
 
-		if (gamepad1.dpad_up) {
-			y = (float)1.0;
-		}
-                if (gamepad1.dpad_down) {
-			y = (float)-1.0;
-		}
-                if (gamepad1.dpad_right) {
-			x = (float)1.0;
-		}
-                if (gamepad1.dpad_left) {
-			x = (float)-1.0;
-		}
-				
-                if (gamepad1.right_bumper) {
-                    fl = (y - x - clockwise)/2;
-                    fr = (y - x + clockwise)/2;
-                    bl = (y + x - clockwise)/2;
-                    br = (y + x + clockwise)/2;
-                } else {
-                    fl = y - x - clockwise;
-                    fr = y - x + clockwise;
-                    bl = y + x - clockwise;
-                    br = y + x + clockwise;
+                if (gamepad1.dpad_up) {
+                    y = (float) - 1.0;
                 }
-				
+                if (gamepad1.dpad_down) {
+                    y = (float) 1.0;
+                }
+                if (gamepad1.dpad_right) {
+                    x = (float) 1.0;
+                }
+                if (gamepad1.dpad_left) {
+                    x = (float) - 1.0;
+                }
+
+                fl = y - x - clockwise;
+                fr = y + x + clockwise;
+                bl = y + x - clockwise;
+                br = y - x + clockwise;
+                
+                if (gamepad1.right_bumper) {
+                    fl /= 2;
+                    fr /= 2;
+                    bl /= 2;
+                    br /= 2;
+                } 
+                    
                 frontLeft.setPower(fl);
                 frontRight.setPower(fr);
                 backLeft.setPower(bl);
                 backRight.setPower(br);
 
-		if (gamepad1.a) {
-		    CarouselSpinner.setPower((float)0.5);
-		    
+                if (gamepad1.a) {
+                    CarouselSpinner.setPower((float)0.95);
+                } else if (gamepad1.b) {
+                    CarouselSpinner.setPower((float)-0.95);
+                } else {
+                    CarouselSpinner.setPower((float)0);                  
+                }
+
                 telemetry.update();
             }
         }
