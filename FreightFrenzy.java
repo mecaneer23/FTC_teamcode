172978fb828829fc393e8a9fa88bb1a11dfddfae
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-11-29 17:57:54 -0600

    Update FreightFrenzy.java

--- FreightFrenzy.java
+++ FreightFrenzy.java
@@ -14,7 +14,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class GabiTest extends LinearOpMode {
+public class FreightFrenzy extends LinearOpMode {
 
     private DcMotor frontLeft;
     private DcMotor backLeft;
