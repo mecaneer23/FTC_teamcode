9b60123b8971d11bdfbca5aa35f6ea4d5d891711
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-11-29 17:58:47 -0600

    Create Simple_Mecanum.java

---
+++ Simple_Mecanum.java
@@ -0,0 +1,90 @@
+/*
+Control Scheme:
+  Gamepad 1 - robot locomotion:
+    left stick - xy position of robot
+    right stick - rotation of robot
+    right bumper 1/2 speed slowmode
+    dpad - 1.0 power in any given direction
+*/
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Simple_Mecanum extends LinearOpMode {
+
+    private DcMotor frontLeft;
+    private DcMotor backLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        float x;
+        float y;
+        float clockwise;
+        double fl;
+        double fr;
+        double bl;
+        double br;
+	    
+        frontLeft = hardwareMap.get(DcMotor.class,
+            "frontLeft");
+        backLeft = hardwareMap.get(DcMotor.class,
+            "backLeft");
+        frontRight = hardwareMap.get(DcMotor.class,
+            "frontRight");
+        backRight = hardwareMap.get(DcMotor.class,
+            "backRight");
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        if (opModeIsActive()) {
+            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            while (opModeIsActive()) {
+                x = gamepad1.left_stick_x;
+                y = gamepad1.left_stick_y;
+                clockwise = gamepad1.right_stick_x;
+
+                if (gamepad1.dpad_up) {
+                    y = (float) - 1.0;
+                }
+                if (gamepad1.dpad_down) {
+                    y = (float) 1.0;
+                }
+                if (gamepad1.dpad_right) {
+                    x = (float) 1.0;
+                }
+                if (gamepad1.dpad_left) {
+                    x = (float) - 1.0;
+                }
+
+                fl = y - x - clockwise;
+                fr = y + x + clockwise;
+                bl = y + x - clockwise;
+                br = y - x + clockwise;
+                
+                if (gamepad1.right_bumper) {
+                    fl /= 2;
+                    fr /= 2;
+                    bl /= 2;
+                    br /= 2;
+                } 
+                    
+                frontLeft.setPower(fl);
+                frontRight.setPower(fr);
+                backLeft.setPower(bl);
+                backRight.setPower(br);
+                    
+                telemetry.update();
+            }
+        }
+    }
+}
