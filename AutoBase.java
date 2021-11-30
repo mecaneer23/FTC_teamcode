1f342c0b6be94f407404ceb3f5884231fd440d6c
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-11-29 19:28:22 -0600

    Update AutoBase.java

--- AutoBase.java
+++ AutoBase.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Auto;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
@@ -43,8 +43,10 @@ public class AutoBase {
     static final double RIGHT_DAMPING_CONSTANT = 0.885;
     static final double DISTANCE_TO_ANGLE_CONSTANT = 1.2;
 
-    // Motor encoder configuration constants
-    static final double PULSES_PER_REVOLUTION = 140;
+    // Motor encoder configuration constant
+    static final double TETRIX_TICKS_PER_MOTOR_REV = 1440;
+    static final double ANDYMARK_TICKS_PER_MOTOR_REV = 1120;
+    static final double PULSES_PER_REVOLUTION = ANDYMARK_TICKS_PER_MOTOR_REV;
     static final double WHEEL_DIAMETER_IN = 4;
     static final double PULSES_PER_IN = PULSES_PER_REVOLUTION / (WHEEL_DIAMETER_IN * 3.1415);
 
@@ -68,16 +70,17 @@ public class AutoBase {
 
         this.telemetry = telemetry;
 
+        ZeroPowerBehaviorBRAKE();
     }
 
-    public void update() {
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        gravity = imu.getGravity();
-        telemetry.addData("Angles: ", angles.toString());
-        telemetry.addData("Gravity: ", gravity.toString());
-    }
+    // public void update() {
+    //     angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    //     gravity = imu.getGravity();
+    //     telemetry.addData("Angles: ", angles.toString());
+    //     telemetry.addData("Gravity: ", gravity.toString());
+    // }
 
-    public void initIMU(HardwareMap hardwareMap) {
+    private static void initIMU(HardwareMap hardwareMap) {
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -91,48 +94,199 @@ public class AutoBase {
         imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
     }
 
-    public void setZeroPowerBehavior() {
+    private static void ZeroPowerBehaviorBRAKE() {
         left_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         right_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         left_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         right_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void setRunUsingEncoder() {
-        left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        left_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        right_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    private static void setRunUsingEncoders() {
+        left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        left_back.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        right_back.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
     }
 
-    public void setRunToPosition() {
+    private static void setRunWithoutEncoders() {
+        left_front.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        right_front.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        left_back.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        right_back.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+    }
+
+    private static void setRunToPosition() {
         left_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         right_front.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         left_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         right_back.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    public void setZeroPower() {
+    private static void resetEncoders() {
+        // method may be stopandreset, but I'm not sure
+        left_front.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        right_front.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        left_back.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        right_back.setMode(DcMotor.RunMode.RESET_ENCODERS);
+    }
+    
+    private static void setTargetPositionsForward(int distance) {
+        left_front.setTargetPosition(distance);
+        right_front.setTargetPosition(distance);
+        left_back.setTargetPosition(distance);
+        right_back.setTargetPosition(distance);
+    }
+
+    private static void setTargetPositionsBackward(int distance) {
+        left_front.setTargetPosition(-distance);
+        right_front.setTargetPosition(-distance);
+        left_back.setTargetPosition(-distance);
+        right_back.setTargetPosition(-distance);
+    }
+
+    private static void setTargetPositionsLeft(int distance) {
+        left_front.setTargetPosition(-distance);
+        right_front.setTargetPosition(distance);
+        left_back.setTargetPosition(distance);
+        right_back.setTargetPosition(-distance);
+    }
+
+    private static void setTargetPositionsRight(int distance) {
+        left_front.setTargetPosition(distance);
+        right_front.setTargetPosition(-distance);
+        left_back.setTargetPosition(-distance);
+        right_back.setTargetPosition(distance);
+    }
+
+    private static void stopDriving() {
         left_front.setPower(0);
         right_front.setPower(0);
         left_back.setPower(0);
         right_back.setPower(0);
     }
 
-    public void
+    private static void goForward(double MotorPower) {
+        left_front.setPower(MotorPower);
+        right_front.setPower(MotorPower);
+        left_back.setPower(MotorPower);
+        right_back.setPower(MotorPower);
+    }
 
-    public void forward(int distanceIN) {
-        while ((PULSES_PER_IN * PULSES_PER_REVOLUTION) != distanceIN) {
-            left_front.setPower(DRIVE_SPEED);
-            right_front.setPower(DRIVE_SPEED);
-            left_back.setPower(DRIVE_SPEED);
-            right_back.setPower(DRIVE_SPEED);
+    private static void goBackward(double MotorPower) {
+        left_front.setPower(-MotorPower);
+        right_front.setPower(-MotorPower);
+        left_back.setPower(-MotorPower);
+        right_back.setPower(-MotorPower);
+    }
+
+    private static void goLeft(double MotorPower) {
+        left_front.setPower(-MotorPower);
+        right_front.setPower(MotorPower);
+        left_back.setPower(MotorPower);
+        right_back.setPower(-MotorPower);
+    }
+
+    private static void goRight(double MotorPower) {
+        left_front.setPower(MotorPower);
+        right_front.setPower(-MotorPower);
+        left_back.setPower(-MotorPower);
+        right_back.setPower(MotorPower);
+    }
+    
+    public void driveForward(double MotorPower, int distanceIN) {
+        if (MotorPower == (double) -2.0) {
+            MotorPower = DRIVE_SPEED;
         }
+        resetEncoders();
+        setTargetPositionsForward(distanceIN);
+        setRunToPosition();
+        goForward(MotorPower);
+        while (
+            left_front.isBusy() &&
+            right_front.isBusy() &&
+            left_back.isBusy() &&
+            right_back.isBusy()
+        ) {
+            // waiting for target position to be reached
+        }
+        stopDriving();
+        setRunUsingEncoders();
+    }
+    
+    public void driveBackward(double MotorPower, int distanceIN) {
+        if (MotorPower == (double) -2.0) {
+            MotorPower = DRIVE_SPEED;
+        }
+        resetEncoders();
+        setTargetPositionsBackward(distanceIN);
+        setRunToPosition();
+        goBackward(MotorPower);
+        while (
+            left_front.isBusy() &&
+            right_front.isBusy() &&
+            left_back.isBusy() &&
+            right_back.isBusy()
+        ) {
+            // waiting for target position to be reached
+        }
+        stopDriving();
+        setRunUsingEncoders();
+    }
+    
+    public void strafeLeft(double MotorPower, int distanceIN) {
+        if (MotorPower == (double) -2.0) {
+            MotorPower = DRIVE_SPEED;
+        }
+        resetEncoders();
+        setTargetPositionsLeft(distanceIN);
+        setRunToPosition();
+        goLeft(MotorPower);
+        while (
+            left_front.isBusy() &&
+            right_front.isBusy() &&
+            left_back.isBusy() &&
+            right_back.isBusy()
+        ) {
+            // waiting for target position to be reached
+        }
+        stopDriving();
+        setRunUsingEncoders();
+    }
+    
+    public void strafeRight(double MotorPower, int distanceIN) {
+        if (MotorPower == (double) -2.0) {
+            MotorPower = DRIVE_SPEED;
+        }
+        resetEncoders();
+        setTargetPositionsRight(distanceIN);
+        setRunToPosition();
+        goRight(MotorPower);
+        while (
+            left_front.isBusy() &&
+            right_front.isBusy() &&
+            left_back.isBusy() &&
+            right_back.isBusy()
+        ) {
+            // waiting for target position to be reached
+        }
+        stopDriving();
+        setRunUsingEncoders();
     }
 }
 
 /*
-    Need 10 methods:
-    Forward, Backward, Left, Right
-    Diagonal (for any direction), Rotation (Left and right)
+    during init:
+    initAuto()
+
+    During run loop:
+    1. driveForward
+    2. driveBackward
+    3. strafeLeft
+    4. strafeRight
+    5. strafeNW
+    6. strafeNE
+    7. strafeSE
+    8. strafeSW
+    9. turnLeft
+    10. turnRight
 */
